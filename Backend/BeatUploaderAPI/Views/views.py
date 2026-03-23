bcd1547eadf4c88c0c3417203f913d1a54022dc3
Piotr Czarnecki <czarnecki.devv@gmail.com>
2026-03-23 17:15:37 +0100

    other/bring the previous file structure
    Changed structure messed up the enviroment

--- Backend/BeatUploaderAPI/Views/views.py
+++ Backend/BeatUploaderAPI/Views/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status as s
 
+from moviepy import ImageClip, AudioFileClip
+
 @api_view(['POST'])
 def videos(request):
     if request.method == "POST":
@@ -14,5 +16,12 @@ def videos(request):
 
         if None in (title, auth_code, image_file, audio_file):
             return Response({'error': '"title", "auth_code", "image_file" or "audio_file" parameter is missing'}, s.HTTP_400_BAD_REQUEST)
+
+        # 1 stworz plik video
+
+
+        # 2 zdobadz tokeny
+
+        # 3 wykonaj zapytanie do YouTube Data API
 
         return Response(status=s.HTTP_200_OK)
